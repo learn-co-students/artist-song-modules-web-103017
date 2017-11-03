75a204d730f1fd819703c659f96498cdb06db961
eshim12 <ellishim@gmail.com>
2017-11-03 11:13:57 -0400

    Complete

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -8,12 +8,17 @@ module Memorable
     def count
       self.all.count
     end
+
+    def find_by_name(name)
+      self.all.detect{|a| a.name == name}
+    end
+
   end
   module InstanceMethods
     def to_param
       name.downcase.gsub(' ', '-')
     end
-    
+
   end
 
 end
